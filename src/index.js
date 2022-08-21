00e89058fde5767c62593fd6e64dce596bf5312a
Levi Barsis <lbarsis@gmail.com>
2022-08-20 17:09:48 -0700

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form')
+  inputForm.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const input = document.querySelector('input#searchByID')
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(resp => resp.json())
+    .then(renderResults)
+
+  })
+}
+
+function renderResults(movie) {
+  const title = document.querySelector('section#movieDetails h4');
+  const summary = document.querySelector('section#movieDetails p');
+
+  title.innerText = movie.title;
+  summary.innerText = movie.summary;
+  movie.reset()
 }
 
 document.addEventListener('DOMContentLoaded', init);
